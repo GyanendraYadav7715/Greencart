0375b2d7391200c1636c50ef6bd7282d711a5f4b
Gyan <gyanendrayadav12345789@gmail.com>
2025-06-09 12:28:30 +0530

    pwa feature is progressing

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), VitePWA({
+    registerType: 'autoUpdate',
+    devOptions: {
+      enabled: true,
+    },
+    manifest: {
+      name: 'Greencart - Grocery Web App',
+      short_name: 'Greencart',
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#0f172a',
+      icons: [
+        {
+          src: ' logo.svg',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'logo.svg',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+      ],
+    },
+  }),],
 })
